bf33bf8f05d777c2bb88ed76f769d3ffbe61dbbe
Hitakrit Goplani <hitakritgoplani@gmail.com>
2022-11-05 22:31:51 +0530

    Small Corrections

--- signup.php
+++ signup.php
@@ -36,7 +36,7 @@ session_start();
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title></title>
+	<title>Book A Laugh</title>
 	<link rel="stylesheet" href="css/style-index.css">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
